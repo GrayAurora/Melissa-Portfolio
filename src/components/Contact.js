54ecbdb423b925c616c8185db313d6c2c438ffaa
GrayAurora <aurora.gitv@gmail.com>
2022-01-01 04:17:47 -0600

    form react submit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,31 +1,44 @@
 import React from 'react';
 import { Form, FormControl, FormGroup, FormLabel, FormText, Button, Row, Col } from "react-bootstrap";
-import{ init, sendForm, emailjs } from '@emailjs/browser';
 
+const initialFormData = Object.freeze({
+  usser_name: "",
+  user_email: "",
+  message: "",
+});
 
+export const Contact = (props) => {
+  const [formData, updateFormData] = React.useState(initialFormData);
 
-export default function Contact() {
-
-  const handleOnSubmit = (e) => {
-    const serviceID = 'portfolio_form';
-    const templateID = 'template_q473ydx';
-    const userID = "user_TanZWr3COe2uwcj8cnb9k";
-
-    e.preventDefault();
-    emailjs.sendForm(
-      serviceID,
-      templateID,
-      e.target,
-      userID
-    )
-    .then(() => {
-      alert('SENT!');
+  const sendFeedback = (serviceId, templateId, variables) => {
+    window.emailjs.send(
+        serviceId, templateId,
+        variables
+    ).then(res => {
+        console.log('Email successfully sent!')
     })
-    .catch((err) => {
-      alert('FAILED...', err);
-    });
+        .catch(err => console.error('There has been an Error.', err))
   }
-  
+
+  const handleChange = (e) => {
+    updateFormData({
+      ...formData,
+
+      [e.target.name]: e.target.value.trim()
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    alert(`Thank you for your message.`);
+    const serviceId = 'portfolio_form';
+    const templateId = 'template_q473ydx';
+
+    sendFeedback(serviceId, templateId, { user_name: formData.user_name, message: formData.message, user_email: formData.user_email })
+
+    console.log(formData);
+    };
+
 
   return (
     <section id="contact">
@@ -35,7 +48,7 @@ export default function Contact() {
             src="./message-bro.svg"
             alt="react logo" />
         <div className="intro formElem">
-          <Form id='form' onSubmit={handleOnSubmit}>
+          <Form id='form' >
             <Row>
               <Col>
                 <h2>Send me a Message!</h2>
@@ -44,25 +57,25 @@ export default function Contact() {
 
             <FormGroup className="formGroup">
               <FormLabel htmlFor="name"> Full Name:</FormLabel><br />
-              <FormControl required  id='name'type="text" name='user_name' placeholder="First and Last Name"/>
+              <FormControl required onChange={handleChange} id='name'type="text" name='user_name' placeholder="First and Last Name"/>
             </FormGroup>
 
 
             <FormGroup className="formGroup">
               <FormLabel htmlFor="email"> Email Address:</FormLabel><br />
-              <FormControl required   id='email' type="email" name='user_email' placeholder="Enter E-mail"/>
+              <FormControl required onChange={handleChange}  id='email' type="email" name='user_email' placeholder="Enter E-mail"/>
               <FormText>Snitches get stitches. Your email is safe with me.</FormText>
             </FormGroup>
 
 
             <FormGroup className="formGroup">
               <FormLabel htmlFor="mssg"> Message:</FormLabel><br />
-              <FormControl required  id='mssg' as='textarea' name='message' rows={5} className='formMssg'placeholder="Enter inquiry details. Be as short or specific as you would like."/>
+              <FormControl required onChange={handleChange} id='mssg' as='textarea' name='message' rows={5} className='formMssg'placeholder="Enter inquiry details. Be as short or specific as you would like."/>
             </FormGroup>
 
             <Row>
               <Col>
-                <Button type='submit' id='button' className="submitBtn" value='Send'>Send Inquiry</Button>
+                <Button onClick={handleSubmit} type='submit' id='button' className="submitBtn" value='Send'>Send Inquiry</Button>
               </Col>
             </Row>
           </Form>
@@ -71,3 +84,5 @@ export default function Contact() {
     </section>
   );
 }
+
+export default Contact;
