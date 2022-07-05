048c15bd19920b1fade112cf2100d1322013e18f
GrayAurora <aurora.gitv@gmail.com>
2022-07-04 21:56:48 -0500

    Updated bio and resume

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,11 @@ export default function About() {
         <div className="intro">
           <FontAwesomeIcon icon={faCode}/>
           <h1>  Hi, I'm Melissa</h1>
-          <p>I am a Front-End Developer for Web and Mobile Applications.</p>
+          <p>
+            One evening, many years ago, I was working as the costumed mouse for the pizza restaurant and entertainment center Chuck E. Cheese. As the night came to a close, I saw a mother and daughter in the play area, and for lack of any other duties, I ventured over to engage in small talk. As we chatted the young girl ran off to the playground, leaving the mother and I. At that moment, I felt something tightly grip my chest and then suddenly my feet were no longer touching the floor. The arcade machines become a blur as I flew through the air, only to immediately come back into focus as I'm thrown to the ground. <br/>
+             <br/> This woman's husband had drunkenly suplex-slammed me to the late 90's blacklight carpet under the assumption I was flirting with his wife. The action had launched my mouse head off, and I, a confused teenage girl, stared back up at him in shock. "Oh I thought you were a guy" he muttered, and walked off. So that's the story of how I got out of the rodent-themed mascot game and into tech. <br/>
+            <br/> My name is Melissa and I'm an aspiring software engineer searching for an opportunity to cultivate my skills in technology and programming. I've spent the past years teaching myself the foundations of different languages and frameworks, attending coding bootcamp, and working on mobile and web-based projects. I enjoy using Figma to plan out and design my screen interfaces to provide further information about app architecture and allow greater control over physical appeal while saving myself time and resources. 
+          </p>
           <div className="btnGrp">
             <Button inline-flex href="#contact">Contact Me!</Button>
             <Button inline-flex href="#projects">My Projects</Button>
